661cb8358b72c2977cc9ddafed1901702b4b82c5
tomas lingotti <tomaslingotti@gmail.com>
2021-05-24 18:39:41 -0300

    add examples for wait and no wait for the response

---
+++ examples/no_wait_for_response.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/tomiok/clipper"
+	"net/http"
+)
+
+func main() {
+	var res *http.Response
+	clipper.Do("my_command", func() error {
+		r, err := http.Get("http://www.google.com/robots.txt")
+		res = r
+		return err
+	}, nil)
+
+	clipper.FillStats("my_command", true)
+}
--- examples/wait_for_response.go
+++ examples/wait_for_response.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func main() {
+func main1() {
 	out := make(chan bool, 1)
 	var res *http.Response
 	valChan := clipper.Do("my_command", func() error {
